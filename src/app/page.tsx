549c1a5ebde1121e0012fe5faa632ad9c2bc2efb
IbrahimMurad <Ibrahimmorad31@gmail.com>
2025-01-11 20:41:33 +0200

    Refactor age calculation logic and improve UI elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,87 @@
 "use client";
 
-import React, { useState } from "react";
+import * as React from "react";
+import { useState } from "react";
 import calculateAge from "./utils/calculateAge";
 
-function DateValue({ dateValue }: { dateValue: number }) {
-  return <span className="text-indigo-500 font-bold">{dateValue}</span>;
+interface Age {
+  years: number;
+  months: number;
+  days: number;
 }
 
+function DateValue({ dateValue }: { dateValue: number }) {
+  return <span className="text-lime-500 font-bold">{dateValue}</span>;
+}
+
+const calculate = (
+  event: React.FormEvent<HTMLFormElement>,
+  setAge: React.Dispatch<React.SetStateAction<Age>>,
+  setError: React.Dispatch<React.SetStateAction<string | null>>,
+  setShowAge: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const birthDateValue = formData.get("birthDate")?.toString();
+  if (!birthDateValue) {
+    setError("Please provide a valid birth date.");
+    return;
+  }
+  const birthDate = new Date(birthDateValue);
+  try {
+    setAge(calculateAge(birthDate));
+    setError(null);
+    setShowAge(true);
+  } catch (err) {
+    setShowAge(false);
+    setError((err as Error).message);
+  }
+};
+
 export default function Home() {
-  const [error, setError] = useState("");
+  const [error, setError] = useState<string | null>(null);
   const [showAge, setShowAge] = useState<boolean>(false);
   const [details, setDetails] = useState<boolean>(false);
-  const [age, setAge] = useState({ years: 0, months: 0, days: 0 });
-  const calculate = (formData: FormData) => {
-    const birthDateValue = formData.get("birthDate")?.toString();
-    if (!birthDateValue) {
-      setError("Please provide a valid birth date.");
-      return;
-    }
-    const birthDate = new Date(birthDateValue);
-    try {
-      setAge(calculateAge(birthDate));
-      setError("");
-      setShowAge(true);
-    } catch (err) {
-      setShowAge(false);
-      setError((err as Error).message);
-    }
-  };
+  const [age, setAge] = useState<Age>({ years: 0, months: 0, days: 0 });
 
   return (
-    <main className="grid place-items-center min-h-screen w-full p-2 bg-slate-900">
-      <div className="w-full max-w-[40rem] bg-slate-700 p-8 rounded-lg">
+    <main className="grid place-items-center min-h-screen w-full p-4 bg-slate-900">
+      <div className="max-w-[40rem] bg-slate-700 p-8 rounded-lg">
         <h1 className="text-3xl font-bold">Age Calculator</h1>
         <p className="mt-4">
-          Calculate you age today by providing you date of birth.
+          Calculate your age today by providing your date of birth.
         </p>
         <form
-          className="my-8 flex justify-center items-center flex-col"
-          action={calculate}
+          className="flex flex-col items-center mt-4"
+          onSubmit={(event) => calculate(event, setAge, setError, setShowAge)}
         >
-          <input
-            type="date"
-            name="birthDate"
-            required
-            className="w-full rounded-lg bg-slate-500 p-2
+          <label className="w-full min-w-30 ">
+            Date of birth:
+            <input
+              type="date"
+              name="birthDate"
+              required
+              className="w-full min-w-30 rounded-lg bg-slate-600 p-2
               border-slate-900 border focus:border-2 focus:border-slate-300
-              focus:outline-none my-4
+              focus:outline-none my-2
               "
-          />
+            />
+          </label>
           <button
             type="submit"
-            className="font-bold text-xl bg-slate-600 p-4 max-w-full w-80 rounded-lg hover:bg-slate-500 hover:border-2 hover:border-slate-700 my-2 h-16"
+            className="font-bold text-xl bg-slate-600 h-12 w-60 rounded-lg hover:bg-slate-500 hover:border-2 hover:border-slate-700 mt-4 mb-2"
           >
             Calculate age
           </button>
         </form>
-        {error && <p className="text-red-700 font-bold">{error}</p>}
+        {error && <p className="text-red-300 font-bold">{error}</p>}
+
         {showAge && (
           <>
             <div className="flex justify-between items-center mt-4">
               <h2 className="text-2xl font-bold">Your age</h2>
               <span
-                className="text-slate-500 underline cursor-pointer"
+                className="text-amber-500 underline cursor-pointer"
                 onClick={() => setDetails(!details)}
               >
                 {details ? "collapse" : "extend"}
@@ -72,12 +91,15 @@ export default function Home() {
               You are <DateValue dateValue={age.years} /> years
               {details && (
                 <>
-                  {" "}
-                  <DateValue dateValue={age.months} /> month, and{" "}
-                  <DateValue dateValue={age.days} /> days
+                  <span className="px-1">
+                    <DateValue dateValue={age.months} /> months, and
+                  </span>
+                  <span className="px-1">
+                    <DateValue dateValue={age.days} /> days
+                  </span>
                 </>
-              )}{" "}
-              old.
+              )}
+              <span> old.</span>
             </p>
           </>
         )}
